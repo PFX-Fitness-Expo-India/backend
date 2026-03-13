8a332a41ef931d02cf8d73f5c4cfa5facc97f819
Siranjeevi619 <siranjeevi0619@gmail.com>
2026-03-13 12:12:24 +0530

    feat: add connectTimeoutMS to MongoDB configuration

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      connectTimeoutMS: 10000,
+      
+    });
     console.log(`Connected to MongoDB: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
