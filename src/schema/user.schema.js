daffaf50fe8ad9c052ed62b4af6e8e513cea7456
Siranjeevi619 <siranjeevi0619@gmail.com>
2026-03-17 23:33:34 +0530

    feat: implement refresh token and logout functionality

--- src/schema/user.schema.js
+++ src/schema/user.schema.js
@@ -25,6 +25,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     enum: ["dev", "athlete", "visitor", "admin"],
   },
+  refreshToken: {
+    type: String,
+    default: null,
+  },
 });
 
 module.exports = userSchema;
