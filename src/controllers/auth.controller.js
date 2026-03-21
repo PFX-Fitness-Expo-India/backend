4f101d1d93ec6030ceebd9d2dea02339d2c99e28
Siranjeevi619 <siranjeevi0619@gmail.com>
2026-03-21 19:28:17 +0530

    refactor: update authentication controller and JWT sign logic

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -55,7 +55,7 @@ const login = async (req, res) => {
     const accessToken = jwt.sign(
       { userId: user._id, role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: process.env.JWT_ACCESS_EXPIRES_IN || "1d" },
+      { expiresIn: process.env.JWT_ACCESS_EXPIRES_IN || "2m" },
     );
 
     const refreshToken = jwt.sign(
