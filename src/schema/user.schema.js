fa4065ccacd1bf288d2e487df5122cbf7fd65ca6
Siranjeevi619 <siranjeevi0619@gmail.com>
2026-04-05 20:11:48 +0530

    feat: add timestamps to user schema for createdAt and updatedAt tracking

--- src/schema/user.schema.js
+++ src/schema/user.schema.js
@@ -1,42 +1,45 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  userId: String,
-  userName: {
-    required: true,
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    userId: String,
+    userName: {
+      required: true,
+      type: String,
+    },
+    phoneNumber: {
+      required: true,
+      type: String,
+      unique: true,
+    },
+    email: {
+      required: true,
+      type: String,
+      unique: true,
+    },
+    password: {
+      required: true,
+      type: String,
+    },
+    role: {
+      required: true,
+      type: String,
+      enum: ["dev", "athlete", "visitor", "admin"],
+    },
+    refreshToken: {
+      type: String,
+      default: null,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: String,
+    verificationTokenExpires: Date,
   },
-  phoneNumber: {
-    required: true,
-    type: String,
-    unique: true,
-  },
-  email: {
-    required: true,
-    type: String,
-    unique: true,
-  },
-  password: {
-    required: true,
-    type: String,
-  },
-  role: {
-    required: true,
-    type: String,
-    enum: ["dev", "athlete", "visitor", "admin"],
-  },
-  refreshToken: {
-    type: String,
-    default: null,
-  },
-  resetPasswordToken: String,
-  resetPasswordExpires: Date,
-  isVerified: {
-    type: Boolean,
-    default: false,
-  },
-  verificationToken: String,
-  verificationTokenExpires: Date,
-});
+  { timestamps: true },
+);
 
 module.exports = userSchema;
